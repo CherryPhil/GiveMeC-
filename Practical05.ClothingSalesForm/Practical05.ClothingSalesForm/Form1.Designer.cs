b7214d2410cd2a49e769fc3e3d9d9f5a120640da
shuanjin99 <33213091+shuanjin99@users.noreply.github.com>
2018-05-20 18:10:26 +0800

    Add files via upload

---
+++ Practical05.ClothingSalesForm/Practical05.ClothingSalesForm/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Practical05.ClothingSalesForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxNofItems = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPrc = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxExtendedPrc = new System.Windows.Forms.TextBox();
+            this.textBoxDiscount = new System.Windows.Forms.TextBox();
+            this.textBoxGST = new System.Windows.Forms.TextBox();
+            this.textBoxAmtDue = new System.Windows.Forms.TextBox();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Clothing Sales ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of Items";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxPrc);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxNofItems);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(36, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(462, 131);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBoxNofItems
+            // 
+            this.textBoxNofItems.Location = new System.Drawing.Point(201, 35);
+            this.textBoxNofItems.Name = "textBoxNofItems";
+            this.textBoxNofItems.Size = new System.Drawing.Size(196, 26);
+            this.textBoxNofItems.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Price";
+            // 
+            // textBoxPrc
+            // 
+            this.textBoxPrc.Location = new System.Drawing.Point(201, 73);
+            this.textBoxPrc.Name = "textBoxPrc";
+            this.textBoxPrc.Size = new System.Drawing.Size(196, 26);
+            this.textBoxPrc.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBoxAmtDue);
+            this.groupBox2.Controls.Add(this.textBoxGST);
+            this.groupBox2.Controls.Add(this.textBoxDiscount);
+            this.groupBox2.Controls.Add(this.textBoxExtendedPrc);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(36, 239);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(462, 208);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Extended Price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "30% Discount";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 20);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "GST (7%)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(102, 20);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Amount Due";
+            // 
+            // textBoxExtendedPrc
+            // 
+            this.textBoxExtendedPrc.Location = new System.Drawing.Point(201, 26);
+            this.textBoxExtendedPrc.Name = "textBoxExtendedPrc";
+            this.textBoxExtendedPrc.ReadOnly = true;
+            this.textBoxExtendedPrc.Size = new System.Drawing.Size(196, 26);
+            this.textBoxExtendedPrc.TabIndex = 4;
+            this.textBoxExtendedPrc.TabStop = false;
+            this.textBoxExtendedPrc.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxDiscount
+            // 
+            this.textBoxDiscount.Location = new System.Drawing.Point(201, 67);
+            this.textBoxDiscount.Name = "textBoxDiscount";
+            this.textBoxDiscount.ReadOnly = true;
+            this.textBoxDiscount.Size = new System.Drawing.Size(196, 26);
+            this.textBoxDiscount.TabIndex = 5;
+            this.textBoxDiscount.TabStop = false;
+            this.textBoxDiscount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxGST
+            // 
+            this.textBoxGST.Location = new System.Drawing.Point(201, 114);
+            this.textBoxGST.Name = "textBoxGST";
+            this.textBoxGST.ReadOnly = true;
+            this.textBoxGST.Size = new System.Drawing.Size(196, 26);
+            this.textBoxGST.TabIndex = 6;
+            this.textBoxGST.TabStop = false;
+            this.textBoxGST.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxAmtDue
+            // 
+            this.textBoxAmtDue.Location = new System.Drawing.Point(201, 156);
+            this.textBoxAmtDue.Name = "textBoxAmtDue";
+            this.textBoxAmtDue.ReadOnly = true;
+            this.textBoxAmtDue.Size = new System.Drawing.Size(196, 26);
+            this.textBoxAmtDue.TabIndex = 7;
+            this.textBoxAmtDue.TabStop = false;
+            this.textBoxAmtDue.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCalc.Location = new System.Drawing.Point(36, 494);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(94, 31);
+            this.buttonCalc.TabIndex = 4;
+            this.buttonCalc.Text = "&Calculate";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonClear.Location = new System.Drawing.Point(149, 494);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(94, 31);
+            this.buttonClear.TabIndex = 5;
+            this.buttonClear.Text = "C&lear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonExit.Location = new System.Drawing.Point(273, 494);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(94, 31);
+            this.buttonExit.TabIndex = 6;
+            this.buttonExit.Text = "E&xit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttonCalc;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonExit;
+            this.ClientSize = new System.Drawing.Size(537, 557);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonCalc);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clothing Sales Practical";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxPrc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxNofItems;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxAmtDue;
+        private System.Windows.Forms.TextBox textBoxGST;
+        private System.Windows.Forms.TextBox textBoxDiscount;
+        private System.Windows.Forms.TextBox textBoxExtendedPrc;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ Practical05.ClothingSalesForm/Practical05.ClothingSalesForm/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practical05.ClothingSalesForm
+{
+    public partial class Form1 : Form
+    {
+        private const decimal discountRate = 0.3m;
+        private const decimal GSTrate = 0.07m;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcTotal(int itemQuantity, decimal price)
+        {
+            decimal extendedPrice = 0, discountAmt = 0;
+
+            extendedPrice = Math.Round(itemQuantity * price, 2);
+            discountAmt = Math.Round(extendedPrice * discountRate, 2);
+
+            textBoxExtendedPrc.Text = extendedPrice.ToString("c");
+            textBoxDiscount.Text = discountAmt.ToString("c");
+
+            compute(extendedPrice, discountAmt);
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            int itemQuantity = 0;
+            decimal price = 0;
+            try
+            {
+                itemQuantity = int.Parse(textBoxNofItems.Text);
+                try
+                {
+                    price = decimal.Parse(textBoxPrc.Text);
+                    calcTotal(itemQuantity, price);
+                }
+                catch
+                {
+                    MessageBox.Show("Invalid Price", "Data Error");
+                    textBoxPrc.Focus();
+                    textBoxPrc.SelectAll();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Invalid Quantity", "Data Error");
+                textBoxNofItems.Focus();
+                textBoxNofItems.SelectAll();
+            }
+        }
+
+        private void compute(decimal extendedPrice, decimal discountAmt)
+        {
+            decimal GST = 0, amtDue = 0;
+
+            GST = Math.Round(extendedPrice * GSTrate, 2);
+            amtDue = Math.Round(extendedPrice - discountAmt + GST, 2);
+
+            textBoxGST.Text = GST.ToString("c");
+            textBoxAmtDue.Text = amtDue.ToString("c");
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxNofItems.Text = "";
+            textBoxNofItems.Clear();
+            textBoxNofItems.Text = string.Empty;
+
+            textBoxPrc.Text = "";
+            textBoxPrc.Clear();
+            textBoxPrc.Text = string.Empty;
+
+            textBoxExtendedPrc.Text = "";
+            textBoxExtendedPrc.Clear();
+            textBoxExtendedPrc.Text = string.Empty;
+
+            textBoxDiscount.Text = "";
+            textBoxDiscount.Clear();
+            textBoxDiscount.Text = string.Empty;
+
+            textBoxGST.Text = "";
+            textBoxGST.Clear();
+            textBoxGST.Text = string.Empty;
+
+            textBoxAmtDue.Text = "";
+            textBoxAmtDue.Clear();
+            textBoxAmtDue.Text = string.Empty;
+
+
+
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
